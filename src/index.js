79cf3178133ceb6db6fb73d5457f8b31316221bc
Demaris Ashmore <demiashmore@yahoo.com>
2022-09-20 13:30:41 -0600

    commit tree ed09fda13a37f2ab4cd63d3fcf5ccb148930e419

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //Nodes
-const menu = () => document.getElementById('ramen-menu')
-const details = () => document.getElementById('ramen-detail')
-const detailImage = () => details().querySelector('img')
-const ramenName = () => details().querySelector('h2')
-const restaurant = () => details().querySelector('h3')
-const rating = () => document.getElementById('rating-display')
-const comment = () => document.getElementById('comment-display')
-const form = () => document.getElementById('new-ramen')
-const editForm = () => document.getElementById('edit-ramen')
-const deleteBtn = () => document.getElementById('delete-btn')
+const menu =  document.getElementById('ramen-menu')
+const details =  document.getElementById('ramen-detail')
+const detailImage =  details.querySelector('img')
+const ramenName =  details.querySelector('h2')
+const restaurant =  details.querySelector('h3')
+const rating =  document.getElementById('rating-display')
+const comment =  document.getElementById('comment-display')
+const form =  document.getElementById('new-ramen')
+const editForm =  document.getElementById('edit-ramen')
+const deleteBtn =  document.getElementById('delete-btn')
 
 //Fetch
 function getRamenImages () {
@@ -27,57 +27,101 @@ function getRamenImages () {
                 const img = document.createElement('img')
                 img.src = ramen.image
                 img.addEventListener('click', () => {
-                    detailImage().src = ramen.image
-                    ramenName().innerText = ramen.name
-                    restaurant().innerText = ramen.restaurant
-                    rating().innerText = ramen.rating
-                    comment().innerText = ramen.comment
+                    detailImage.src = ramen.image
+                    ramenName.innerText = ramen.name
+                    restaurant.innerText = ramen.restaurant
+                    rating.innerText = ramen.rating
+                    comment.innerText = ramen.comment
                 })
-                menu().appendChild(img)
+                // deleteBtn.addEventListener('click', deleteRamen)
+                menu.appendChild(img)
             })
         })
 }
+
+function postNewRamen (ramenObj) {
+    fetch('http://localhost:3000/ramens', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(ramenObj)
+    })
+        .then(res => res.json())
+        .then(newRamen => {
+            detailImage.src = newRamen.image
+            ramenName.innerText = newRamen.ramenName
+            restaurant.innerText = newRamen.restaurant
+            rating.innerText = newRamen.rating
+            comment.innerText = newRamen.comment
+
+            const img = document.createElement('img')
+            img.src = newRamen.image
+            menu.appendChild(img)
+        })
+}
+
+function patchRamen () {
+    fetch(`http://localhost:3000/ramens`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify()
+    })
+        .then(res => res.json())
+        .then(res => console.log(res))
+}
+
 
 function loadFirstRamen () {
     fetch('http://localhost:3000/ramens/1')
         .then(res => res.json())
         .then(ramen => {
-            detailImage().src = ramen.image
-            ramenName().innerText = ramen.name
-            restaurant().innerText = ramen.restaurant
-            rating().innerText = ramen.rating
-            comment().innerText = ramen.comment
+            detailImage.src = ramen.image
+            ramenName.innerText = ramen.name
+            restaurant.innerText = ramen.restaurant
+            rating.innerText = ramen.rating
+            comment.innerText = ramen.comment
         })
 }
 
 
 //Form Submissions
-form().addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log(e.target)
-    detailImage().src = document.getElementById('new-image').value
-    ramenName().innerText = document.getElementById('new-name').value
-    restaurant().innerText = document.getElementById('new-restaurant').value
-    rating().innerText = document.getElementById('new-rating').value
-    comment().innerText = document.getElementById('new-comment').value
+    let ramenObj = {
+        image : document.getElementById('new-image').value,
+        ramenName : document.getElementById('new-name').value,
+        restaurant : document.getElementById('new-restaurant').value,
+        rating : document.getElementById('new-rating').value,
+        comment : document.getElementById('new-comment').value
+    }
 
-    form().reset()
+    postNewRamen(ramenObj)
+    form.reset()
 })
 
-editForm().addEventListener('submit', e => {
+editForm.addEventListener('submit', e => {
     e.preventDefault()
-    rating().innerText = document.getElementById('new-rating2').value
-    comment().innerText = document.getElementById('new-comment2').value
 
-    editForm().reset()
+    rating.innerText = document.getElementById('new-rating2').value
+    comment.innerText = document.getElementById('new-comment2').value
+
+    console.log(details.value)
+
+    patchRamen()
+    editForm.reset()
 })
 
-//Delete Button
-deleteBtn().addEventListener('click', () => {
-    // detailImage().src = ' '
-    // ramenName().innerText = ' '
-    // restaurant().innerText = ' '
-    // rating().innerText = ' '
-    // comment().innerText = ' '
-    loadFirstRamen()
-})
+// // Delete Button
+// function deleteRamen() {
+//     // menu.lastChild.remove()
+//     while (menu.lastChild) {
+//        menu.lastChild.remove();
+//     }
+//     menu.map((ramen) => {
+//         addRamenToMenu(ramen);
+//     });
+// }
